244fbedaf06a67d97c45530593e87449889bb982
kasun789 <kasungayashan789@gmail.com>
2023-11-09 09:47:18 +0530

    create a login component

--- FoodMania/src/app/app-routing.module.ts
+++ FoodMania/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+// set routes
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
--- FoodMania/src/app/app.module.ts
+++ FoodMania/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
